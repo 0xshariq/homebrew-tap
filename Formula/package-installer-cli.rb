b7fbab81bace84501ab81205c655e7653f3c6379
Sharique Chaudhary <khanshariq92213@gmail.com>
2025-09-28 16:05:56 +0000

    Add Homebrew Formula

--- Formula/package-installer-cli.rb
+++ Formula/package-installer-cli.rb
@@ -6,20 +6,20 @@ class PackageInstallerCli < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.0.0/package-installer-cli-1.1.0-darwin-arm64.tar.gz"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.2.0/package-installer-cli-1.2.0-darwin-arm64.tar.gz"
       sha256 "aa5b5f7977e68fe968cae36d9dea851d630e59ea817d22b9d7e211351483cb77"
     else
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.0.0/package-installer-cli-1.1.0-darwin-amd64.tar.gz"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.2.0/package-installer-cli-1.2.0-darwin-amd64.tar.gz"
       sha256 "a7b2c6d2abb7e0f41ef3922480fa21c7d5cda5bf96b198a9acd5aec3194c2016"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.0.0/package-installer-cli-1.1.0-linux-arm64.tar.gz"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.2.0/package-installer-cli-1.2.0-linux-arm64.tar.gz"
       sha256 "0f43b309a2f96a059edaa795a32dc442c97e0bbb1c5d79b9f9ba23d910e2359a"
     else
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.0.0/package-installer-cli-1.1.0-linux-amd64.tar.gz"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.2.0/package-installer-cli-1.2.0-linux-amd64.tar.gz"
       sha256 "a8a6412741d750ac159d879b7537288d152cf832757e9e37747edec78d51606f"
     end
   end
--- Formula/pi.rb
+++ Formula/pi.rb
@@ -6,20 +6,20 @@ class Pi < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.0.0/pi-1.1.0-darwin-arm64.tar.gz"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.2.0/pi-1.2.0-darwin-arm64.tar.gz"
       sha256 "70e31fa74a6ecb9e19ef3dfee708065b5abb5c30d5edba4f3d62f74067848b96"
     else
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.0.0/pi-1.1.0-darwin-amd64.tar.gz"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.2.0/pi-1.2.0-darwin-amd64.tar.gz"
       sha256 "b5f2b671750f8680f6b559e6f2878b01242addbc0f306953b7d44b8db33f283f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.0.0/pi-1.1.0-linux-arm64.tar.gz"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.2.0/pi-1.2.0-linux-arm64.tar.gz"
       sha256 "dcf58cae92ff11f15622defc3e851f218273e06dde30632b26e2933b8b5b9450"
     else
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.0.0/pi-1.1.0-linux-amd64.tar.gz"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.2.0/pi-1.2.0-linux-amd64.tar.gz"
       sha256 "74e8ec4c4716668321764bf10840347dc6ae2d611f483e3a3e3ffc83c904c7d2"
     end
   end
