4e4c54025c1bdd24711078d4eeabf4da7d188850
Sharique Chaudhary <khanshariq92213@gmail.com>
2025-10-01 18:30:02 +0000

    Add homebrew formula

--- Formula/package-installer-cli.rb
+++ Formula/package-installer-cli.rb
@@ -1,26 +1,26 @@
 class PackageInstallerCli < Formula
   desc "CLI tool to install and manage packages easily"
   homepage "https://github.com/0xshariq/homebrew-tap"
-  version "1.3.1"
+  version "1.3.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.1/package-installer-cli-1.3.1-darwin-arm64.tar.gz"
-      sha256 "5e3b9ca093259afbb965a93f57eddb8bea7c5e1e9c53fd50cc1a009d7259fb08"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.2/package-installer-cli-1.3.2-darwin-arm64.tar.gz"
+      sha256 "8c5f9ef2e4d9c780325a050f2ad02ebf35d35f5d4b8aef47c6d1b75cceebff6b"
     else
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.1/package-installer-cli-1.3.1-darwin-amd64.tar.gz"
-      sha256 "cf40a866d783ce538e38b6aa521a0cae58dc730d244a03d94693880a3dc7a6a7"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.2/package-installer-cli-1.3.2-darwin-amd64.tar.gz"
+      sha256 "ccecfd642a16c0fd1427c5b90960db3f46084add79d4d998c6ffea21487f9247"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.1/package-installer-cli-1.3.1-linux-arm64.tar.gz"
-      sha256 "9e9b6014f1941364fcf26caa900ac32faaed9f25fc4ad3636aa3a42f7a13ca9d"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.2/package-installer-cli-1.3.2-linux-arm64.tar.gz"
+      sha256 "a6dc640df366323a9710284bb0bd6a36623eece86ef7c8f070a4d3623b37bba6"
     else
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.1/package-installer-cli-1.3.1-linux-amd64.tar.gz"
-      sha256 "40a435f458a1773774923f7eb65cb111d20a7338a109c3e7f6b7ed5a801f274f"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.2/package-installer-cli-1.3.2-linux-amd64.tar.gz"
+      sha256 "b52e998ce9666125c7c0f51553dc8529e480b8933bec55569d627ba44dc8c6ee"
     end
   end
 
--- Formula/pi.rb
+++ Formula/pi.rb
@@ -1,26 +1,26 @@
 class Pi < Formula
   desc "Second option of Package Installer CLI"
   homepage "https://github.com/0xshariq/homebrew-tap"
-  version "1.3.1"
+  version "1.3.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.1/pi-1.3.1-darwin-arm64.tar.gz"
-      sha256 "fb38342e4963949c61c3efe3271d1025bff6e0b8f5131c22b9d87da68469366c"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.2/pi-1.3.2-darwin-arm64.tar.gz"
+      sha256 "0f73eadd1c0d41493f6c996528338c18257eafee997ccb354e0ddb1d1f2e4c66"
     else
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.1/pi-1.3.1-darwin-amd64.tar.gz"
-      sha256 "926355fe7dbde441f6df03e264b7623af61e3102f309c43e28df465b45d2e54d"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.2/pi-1.3.2-darwin-amd64.tar.gz"
+      sha256 "c2a87828edc9d8ab011065c6a5a370731c6afe6af58ba391be7860b276076576"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.1/pi-1.3.1-linux-arm64.tar.gz"
-      sha256 "fc82d5b3eca5b7940a721fb611bbfdb565d69df43b3604fa852cb0299cd268d3"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.2/pi-1.3.2-linux-arm64.tar.gz"
+      sha256 "660a787efd77786eeda6cb6c80d0f8907992c012aba6ecdb450b5abdb6d2996e"
     else
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.1/pi-1.3.1-linux-amd64.tar.gz"
-      sha256 "ac2831eed501fec6cec0f3bd4b5189e1e1e478ac270d05bc24dbca86a77cd2ce"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v1.3.2/pi-1.3.2-linux-amd64.tar.gz"
+      sha256 "f9ed85a1561c69f1a6a8902918a49e31ae871e3ef577e1b2b8a13365f5351cc7"
     end
   end
 
