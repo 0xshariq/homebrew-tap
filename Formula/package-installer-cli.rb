6f298e2fc8bca283b95d6c9b792fc3652a77ccb3
Sharique Chaudhary <khanshariq92213@gmail.com>
2025-10-07 20:32:53 +0000

    Add homebrew formula

--- Formula/package-installer-cli.rb
+++ Formula/package-installer-cli.rb
@@ -1,26 +1,26 @@
 class PackageInstallerCli < Formula
   desc "CLI tool to install and manage packages easily"
   homepage "https://github.com/0xshariq/homebrew-tap"
-  version "2.0.0"
+  version "2.1.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.0.0/package-installer-cli-2.0.0-darwin-arm64.tar.gz"
-      sha256 "3a2fa9b66222b9a86e6855e7d6695457212370bba8852fbbb28947695247a27f"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.1.0/package-installer-cli-2.1.0-darwin-arm64.tar.gz"
+      sha256 "20d3470eb5da75a77ca0d124da72bb6d4cba75b39b3af06c15114696444a862e"
     else
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.0.0/package-installer-cli-2.0.0-darwin-amd64.tar.gz"
-      sha256 "172942f7b7e8ece4a33275e094150913560cc7844749c55a267fd46c4e64e522"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.1.0/package-installer-cli-2.1.0-darwin-amd64.tar.gz"
+      sha256 "c157fcbba9039c8d951e289d58862b77a97a1adb15c8659b413eb24561711ec0"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.0.0/package-installer-cli-2.0.0-linux-arm64.tar.gz"
-      sha256 "140afc5daaf7e00b3c019d19f724bc3d2e90c4e2f011b282268baa4429644bc9"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.1.0/package-installer-cli-2.1.0-linux-arm64.tar.gz"
+      sha256 "ccdb07d775ff0040273b7d5dc3d63ab80d202f32e7ea0b5dd6cd213593df2a53"
     else
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.0.0/package-installer-cli-2.0.0-linux-amd64.tar.gz"
-      sha256 "b2fc3c1a889b20d33a58a04eb04fcc92f267ed28b96f5bf04513a82845ffadec"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.1.0/package-installer-cli-2.1.0-linux-amd64.tar.gz"
+      sha256 "77a1655078108e888af867fda7da36c3da3f3def5e17f892828bbf2b2bc32cfb"
     end
   end
 
--- Formula/pi.rb
+++ Formula/pi.rb
@@ -1,26 +1,26 @@
 class Pi < Formula
   desc "Second option of Package Installer CLI"
   homepage "https://github.com/0xshariq/homebrew-tap"
-  version "2.0.0"
+  version "2.1.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.0.0/pi-2.0.0-darwin-arm64.tar.gz"
-      sha256 "35b9ba371799254524d4b4f33a616b81261e5df5ff1bbc63661a2fec1bb2f226"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.1.0/pi-2.1.0-darwin-arm64.tar.gz"
+      sha256 "2c664ef5e27379591cb7eff4cf432355c17c798742ad8046a760035eefafe26e"
     else
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.0.0/pi-2.0.0-darwin-amd64.tar.gz"
-      sha256 "f79cbfb3f165b59fba329f860a48bb32f577ec93b95e3a286c32320777017ec8"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.1.0/pi-2.1.0-darwin-amd64.tar.gz"
+      sha256 "7e9d58b6923cd700bd7efa678496e5966a50130db8689f446200304a0f24cb5f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.0.0/pi-2.0.0-linux-arm64.tar.gz"
-      sha256 "2233ecb9858ac6129a0cc37e3b5d12bff56d8c0476f17e05cdcf250a3635a366"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.1.0/pi-2.1.0-linux-arm64.tar.gz"
+      sha256 "bca1a317b625d43e20dca2cd8cc305429472ad3feafb596003dc09f227c684af"
     else
-      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.0.0/pi-2.0.0-linux-amd64.tar.gz"
-      sha256 "a959385bdf95266d034d2474ec73315e7aec39fec97fc4e8195de9a0ce117318"
+      url "https://github.com/0xshariq/homebrew-tap/releases/download/v2.1.0/pi-2.1.0-linux-amd64.tar.gz"
+      sha256 "b8b57694fc57430e20e134e51046cbe3269c6eb1bb7155793fe229181006e98b"
     end
   end
 
